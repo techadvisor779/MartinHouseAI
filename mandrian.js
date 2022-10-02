23053a33918940a8d5410470a28ea39bae0b5987
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-01 19:55:07 -0700

    Update mandrian.js

--- mandrian.js
+++ mandrian.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById('canvas1')
 canvas.width = innerWidth;
 canvas.height = innerHeight-135;
 var c = canvas.getContext('2d');
-var dataUrl = canvas.toDataURL("image/png");
 
 var img = new Image();
 img.crossOrigin = "Anonymous";
@@ -20,41 +19,13 @@ const pallet0 = [
 
 const button = document.getElementById('button');
 
-var putPoint = function (e, dragging, dribble_COLORS, COLORS) {
-    if (e.clientX > 410) {
-        if (e.clientX < 1400) {
-            if (e.clientY > 90) {
-                if (e.clientY < 676 - radius) {
-                    c.beginPath();
- 
-}
-
 var change_pallet1 = function () {
     c.fillStyle = 'White';
     c.fillRect(200, 100, 1000, 700);
     c.strokeStyle = 'Black';
     c.strokeRect(200, 100, 1000, 700);
     c.fillStyle='White';
-
 }
-
-var change_bg = function () {
-    c.fillStyle = bg_color;
-    c.fillRect(400, 1, 1000, 599);
-    c.fill();
-}
-
-var engage = function (e) {
-    dragging = true;
-    radius = 14;
-    putPoint(e);
-    console.log(e.clientX, e.clientY);
-}
-
-var disengage = function (e) {
-    dragging = false;  
-   
-}    
 
 function assignColors(COLORS) {    
     
