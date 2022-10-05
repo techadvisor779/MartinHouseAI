6f2a3b84b8fa76eb23e1f449c1916bf471185456
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-04 21:52:02 -0700

    Update pollock.js

--- pollock.js
+++ pollock.js
@@ -112,22 +112,23 @@ var putPoint = function (e, dragging, dribble_COLORS, COLORS) {    ///  400, 0, 
 var dribble = function (e) {
     temp = c.fillStyle;
     dribble_count += 1;
-    if (dribble_count > 1) {
+    if (dribble_count >= 1) {
         ranX = (Math.random() * 160) - 80;
         ranY = (Math.random() * 160) - 80;
         ranR1 = Math.random() * 10;
         ranR2 = Math.random() * 10;
         if (e.clientX > 400 + ranX) {
             if (e.clientX + ranX < 1000) {
-                if (e.clientY + ranY > 90) {
-                    if (e.clientY + ranY < 676 - radius) {
+                if (e.clientY + ranY > 120) {
+                    if (e.clientY + ranY < 700 - radius) {
                         c.beginPath();
                         if (randColor_Opt) {
                             ranC = Math.round(Math.random() * 7);
                             c.fillStyle = dribble_COLORS[ranC];
                         }
                         c.arc(e.clientX + ranX, e.clientY + ranY - 75, ranR1, 0, Math.PI * 2);
-                        c.arc(600 - e.clientX + ranX, 670 - e.clientY + ranY, ranR2, 0, Math.PI * 2);
+                        c.fill();
+                        c.arc(600 - e.clientX + ranX, 700 - e.clientY + ranY, ranR2, 0, Math.PI * 2);
                         c.fill();
                         dribble_count = 0;
                     }
