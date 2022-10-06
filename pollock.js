2d1b7da964badf4ab1fef497dce8fad0b6240ba9
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-05 20:27:42 -0700

    Update pollock.js

--- pollock.js
+++ pollock.js
@@ -83,7 +83,7 @@ const arr = new Uint8ClampedArray(40_000);
 var putPoint = function (e, dragging, dribble_COLORS, COLORS) {    ///  400, 0, 1000, 600);
     console.log( e.clientX, e.clientY);
     delta = (e.clientX * e.clientX) + (e.clientY * e.clientY);
-    if (e.clientX > 401) {
+    if (e.clientX > 401 - radius) {
         if (e.clientX < 1000) {
             if (e.clientY > 0) {
                 if (e.clientY < 730 - radius) {
