c8acde60f4774fa9dd61cae335008b6b31ffa952
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-06 18:56:07 -0700

    Update mandrian.js

--- mandrian.js
+++ mandrian.js
@@ -3,15 +3,18 @@ canvas.width = innerWidth;
 canvas.height = innerHeight-135;
 var c = canvas.getContext('2d');
 
+const rects = [];
+
 const pallet0 = [
      'white',               //white  0
      '#fff001',             //yellow   1
      '#ff0101',             //red    2
      '#0101fd',             //blue    3
      'Black'                //black 7
-]	
+]
 
-var change_pallet1 = function () {
+
+function MyBorders() {
     c.fillStyle = 'White';
     c.fillRect(100, 0, 280, 600);
     c.strokeStyle = 'Black';
@@ -19,14 +22,7 @@ var change_pallet1 = function () {
     c.fillStyle='White';
     c.fillRect(400, 0, 1000, 600);
     c.strokeStyle = 'Black';
-    c.strokeRect(400, 0, 1000, 600);
-
-    c.fillStyle = 'Black';
-    c.font = "16px Georgia";
-    c.fillText("Paint Brush", 200, 46);
-    c.fillStyle = 'Black';
-    c.strokeRect(181, 56, 28, 28);
-    c.fillStyle = 'DarkOliveGreen';
+    c.strokeRect(400, 0, 1000, 600);    
 }
 
 const container1 = document.querySelectAll(".range-slider");
@@ -54,5 +50,5 @@ console.log(slider);
 //});
 
 customSlider();
-change_pallet1();
+MyBorders();
 
