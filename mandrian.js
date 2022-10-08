6167c5780f8adfe67ad444ddac065876e1d0646d
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-07 20:05:54 -0700

    Update mandrian.js

--- mandrian.js
+++ mandrian.js
@@ -1,81 +1,108 @@
-const canvas = document.getElementById('canvas1')
-canvas.width = 800;
-canvas.height = 600;
-var c = canvas.getContext('2d');
-c.lineWidth = 10;
+var canvas = document.querySelector('canvas');
+var context = canvas.getContext('2d');
 
-const rectangles = [];
+var size = 320;
+var dpr = window.devicePixelRatio;
+canvas.width = size * dpr;
+canvas.height = size * dpr;
+context.scale(dpr, dpr);
+context.lineWidth = 8;
+var step = size / 7;
+var white = '#F2F5F1';
+var colors = ['#D40920', '#1356A2', '#F7D842']
 
-var splitDirectionVert = true;
+var squares = [{
+    x: 0,
+    y: 0,
+    width: size,
+    height: size
+  }];
 
-canvas.addEventListener('click', onRectangleClick);
+function splitSquaresWith(coordinates) {
+  const { x, y } = coordinates;
 
-function createRectangles(x,y,width,height) {
-     rectangles.push({x,y,width,height})     
+  for (var i = squares.length - 1; i >= 0; i--) {
+  const square = squares[i];
+
+  if (x && x > square.x && x < square.x + square.width) {
+      if(Math.random() > 0.5) {
+        squares.splice(i, 1);
+        splitOnX(square, x); 
+      }
+  }
+
+  if (y && y > square.y && y < square.y + square.height) {
+      if(Math.random() > 0.5) {
+        squares.splice(i, 1);
+        splitOnY(square, y); 
+      }
+  }
+  }
 }
 
-function drawRectangles() {
-     rectangles.forEach((rectangle) => {
-          c.beginPath();
-          c.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-          c.closePath();
-          c.stroke();
-     });
+function splitOnX(square, splitAt) {
+  var squareA = {
+    x: square.x,
+    y: square.y,
+    width: square.width - (square.width - splitAt + square.x),
+    height: square.height
+  };
+
+  var squareB = {
+  x: splitAt,
+  y: square.y,
+  width: square.width - splitAt + square.x,
+  height: square.height
+  };
+
+  squares.push(squareA);
+  squares.push(squareB);
 }
 
-function onRectangleClick(e) {     
-     const ClickedIndex = rectangles.findIndex((rectange) => {
-          if(
-               e.x > rectangle.x &&
-               e.x < rectangle.x + rectangle.width &&
-               e.y > rectangle.y &&
-               e.y < rectangle.y + rectangle.height               
-            ) {
-               return true;
-          }
-     });
-                                               
-     const clickedRectangle = rectangles[ClickedIndex];
-     
-     rectangles.splice(clickedIndex, 1);
-     splitRectangleAt(clickedRectangle, {
-          x: e.x - clickedRectangle.x,
-          y: e.y - clickedRectangle.y,
-     });     
-}         
+function splitOnY(square, splitAt) {
+  var squareA = {
+    x: square.x,
+    y: square.y,
+    width: square.width,
+    height: square.height - (square.height - splitAt + square.y)
+  };
 
-function splitRectangleAt( clickedRectangle, position) {
-     if (splitDirectionVert) {          
-          rectangles.push({
-               x: rectangle.x,
-               y: rectangle.y,
-               width: position.x,
-               height: rectangle.height,
-               color: getColor()
-          });
-          rectangles.push({
-               x: rectangle.x + position.x,
-               y: rectangle.y,      
-               width: rectangle.width -position.x,
-               height: rectangle.height,
-               color: getColor()
-          });
-     } else {
-          rectangles.push({
-               x: rectangle.x,
-               y: rectangle.y,
-               width: rectangle.width,
-               height: position.y,
-               color: getColor()
-          });
-          rectangles.push({
-               x: rectangle.x,
-               y: rectangle.y,
-               width: rectangle.width,
-               height: rectangle.height - position.y,
-          });
-     } 
-     splitDirectionVertical = !splitDirectionVertical;
-     drawRectangles();        
+  var squareB = {
+  x: square.x,
+  y: splitAt,
+  width: square.width,
+  height: square.height - splitAt + square.y
+  };
+
+  squares.push(squareA);
+  squares.push(squareB);
 }
 
+for (var i = 0; i < size; i += step) {
+  splitSquaresWith({ y: i });
+  splitSquaresWith({ x: i });
+}
+
+function draw() {
+  for (var i = 0; i < colors.length; i++) {
+    squares[Math.floor(Math.random() * squares.length)].color = colors[i];
+  }
+  for (var i = 0; i < squares.length; i++) {
+    context.beginPath();
+    context.rect(
+      squares[i].x,
+      squares[i].y,
+      squares[i].width,
+      squares[i].height
+    );
+    if(squares[i].color) {
+      context.fillStyle = squares[i].color;
+    } else {
+      context.fillStyle = white
+    }
+    context.fill()
+    context.stroke();
+  }
+}
+
+draw()
