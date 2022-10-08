f4252bc0e43a742a32dc0967a028c445c297b9e2
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-08 12:26:37 -0700

    Update mandrian.js

--- mandrian.js
+++ mandrian.js
@@ -1,8 +1,8 @@
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 
-var w = 600;
-var h = 450;
+var w = canvas.width;
+var h = canvas.height;
 var dpr = window.devicePixelRatio;
 c.scale(dpr, dpr);
 c.lineWidth = 4;
