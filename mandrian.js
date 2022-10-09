6b0b58c5b7d6ae1d8d3ef9b3d623db423f639c3b
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-09 12:12:02 -0700

    Update mandrian.js

--- mandrian.js
+++ mandrian.js
@@ -106,8 +106,8 @@ var Trad = document.getElementById('Traditional').value;
 var Pastel = document.getElementById('Pastel').value;
 
 LineID.addEventListener('click', function() {
-    document.getElementById('LineID').value = 'true');
-    document.getElementById('PaintID').value = 'false');  
+    document.getElementById('LineID').value = 'true';
+    document.getElementById('PaintID').value = 'false';  
     console.log(LineID);
 });
 
