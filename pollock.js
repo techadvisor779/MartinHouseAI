71632f56ad1cc0b8b5529caeabf278b323ab6acd
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-11 18:38:39 -0700

    Update pollock.js

--- pollock.js
+++ pollock.js
@@ -90,7 +90,7 @@ var putPoint = function (e, dragging, dribble_COLORS, COLORS) {    ///  400, 0, 
                     c.beginPath();
                     c.arc(e.clientX, e.clientY - 125, radius, 0, Math.PI * 2);
                     c.fill();
-                    c.arc(1800 - e.clientX, 730 - e.clientY, radius, 0, Math.PI * 2);
+                    c.arc(1800 - e.clientX, 750 - e.clientY, radius, 0, Math.PI * 2);
                     c.fill();
                 }
                 else dragging = false;
