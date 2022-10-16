a950d5488ddda073a836b6fbeadcbdf70c80de42
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-15 21:52:34 -0700

    Update mandrian.js

--- mandrian.js
+++ mandrian.js
@@ -1,12 +1,12 @@
 var canvas = document.querySelector('canvas');
-var context = canvas.getContext('2d');
+var c = canvas.getContext('2d');
 
 var size = 320;
 var dpr = window.devicePixelRatio;
 canvas.width = size * dpr;
 canvas.height = size * dpr;
 context.scale(dpr, dpr);
-context.lineWidth = 6;
+c.lineWidth = 6;
 var step = size / 7;
 var white = '#F2F5F1';
 var colors = ['#D40920', '#1356A2', '#F7D842']
@@ -29,84 +29,83 @@ function splitSquaresWith(coordinates) {
   for (var i = squares.length - 1; i >= 0; i--) {
   const square = squares[i];
 
-  if (x && x > square.x && x < square.x + square.width) {
-      if(Math.random() > 0.5) {
-        squares.splice(i, 1);
-        splitOnX(square, x); 
-      }
-  }
+    if (x && x > square.x && x < square.x + square.width) {
+        if(Math.random() > 0.5) {
+            squares.splice(i, 1);
+            splitOnX(square, x); 
+        }
+    }
 
-  if (y && y > square.y && y < square.y + square.height) {
-      if(Math.random() > 0.5) {
-        squares.splice(i, 1);
-        splitOnY(square, y); 
-      }
-  }
-  }
+    if (y && y > square.y && y < square.y + square.height) {
+        if(Math.random() > 0.5) {
+            squares.splice(i, 1);
+            splitOnY(square, y); 
+        }
+    }
 }
 
 function splitOnX(square, splitAt) {
-  var squareA = {
-    x: square.x,
-    y: square.y,
-    width: square.width - (square.width - splitAt + square.x),
-    height: square.height
-  };
+    var squareA = {
+        x: square.x,
+        y: square.y,
+        width: square.width - (square.width - splitAt + square.x),
+        height: square.height
+    };
 
-  var squareB = {
-  x: splitAt,
-  y: square.y,
-  width: square.width - splitAt + square.x,
-  height: square.height
-  };
+    var squareB = {
+        x: splitAt,
+        y: square.y,
+        width: square.width - splitAt + square.x,
+        height: square.height
+    };
 
-  squares.push(squareA);
-  squares.push(squareB);
+    squares.push(squareA);
+    squares.push(squareB);
 }
 
 function splitOnY(square, splitAt) {
-  var squareA = {
-    x: square.x,
-    y: square.y,
-    width: square.width,
-    height: square.height - (square.height - splitAt + square.y)
-  };
+    var squareA = {
+        x: square.x,
+        y: square.y,
+        width: square.width,
+        height: square.height - (square.height - splitAt + square.y)
+    };
 
-  var squareB = {
-  x: square.x,
-  y: splitAt,
-  width: square.width,
-  height: square.height - splitAt + square.y
-  };
+    var squareB = {
+        x: square.x,
+        y: splitAt,
+        width: square.width,
+        height: square.height - splitAt + square.y
+    };
 
-  squares.push(squareA);
-  squares.push(squareB);
+    squares.push(squareA);
+    squares.push(squareB);
 }
 
 for (var i = 0; i < size; i += step) {
-  splitSquaresWith({ y: i });
-  splitSquaresWith({ x: i });
+    splitSquaresWith({ y: i });
+    splitSquaresWith({ x: i });
 }
 
 function draw() {
-  for (var i = 0; i < colors.length; i++) {
-    squares[Math.floor(Math.random() * squares.length)].color = colors[i];
-  }
-  for (var i = 0; i < squares.length; i++) {
-    context.beginPath();
-    context.rect(
-      squares[i].x,
-      squares[i].y,
-      squares[i].width,
-      squares[i].height
+    for (var i = 0; i < colors.length; i++) {
+        squares[Math.floor(Math.random() * squares.length)].color = colors[i];
+    }
+    for (var i = 0; i < squares.length; i++) {
+        c.beginPath();
+        c.rect(
+        squares[i].x,
+        squares[i].y,
+        squares[i].width,
+        squares[i].height
     );
     if(squares[i].color) {
-      context.fillStyle = squares[i].color;
+        c.fillStyle = squares[i].color;
     } else {
-      context.fillStyle = white
+        c.fillStyle = white
     }
-    context.fill()
-    context.stroke();
+    c.fill()
+    c.stroke();
   }
 }
 
