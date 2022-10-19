5eb55c4463c3d77b01e7978d462d22528b5aa487
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-18 21:45:03 -0700

    Update mandrian.js

--- mandrian.js
+++ mandrian.js
@@ -116,11 +116,12 @@ var draw = function(e) {
     } 
     if (PaintID.checked) {             
         for (var i = 0; i < squares.length; i++) {  
-            console.log(e.clientX, e.clientY, squares[i].x, squares[i].y)
             if (e.clientX-350 > squares[i].x && e.clientX-350 < squares[i].x + squares[i].width && e.clientY-200 > squares[i].y && e.clientY-200 < squares[i].y + squares[i].height) {
+                console.log("true", e.clientX, e.clientY,)
                 colorsChng = Math.random(6);
                 squares[i].color = colors[colorsChng];
                 c.fill();
+                c.stroke();
             }
         }
     }
