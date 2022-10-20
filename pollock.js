edad685d0eb35271168e303178263865a1ff964b
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-19 21:12:39 -0700

    Update pollock.js

--- pollock.js
+++ pollock.js
@@ -549,7 +549,7 @@ function submitForm() {
     var val = document.getElementById('signIt').value;
     c.fillStyle = 'Black'
     c.font = "18pt papyrus";
-    c.text-align = 'right';
+    c.text-align = right;
     c.fillText(val, 1300, 550);
 }
 
