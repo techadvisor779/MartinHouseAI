abb938db913d08e2d56c23a8184c212781bca5d3
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-20 18:17:24 -0700

    Update pollock.js

--- pollock.js
+++ pollock.js
@@ -551,6 +551,7 @@ function submitForm() {
     c.fillStyle = 'Black'
     c.font = "18pt papyrus";
     c.textAlign = 'right';
+    c.transform: rotate(10deg);
     c.fillText(val, 1320, 550);
 }
 
