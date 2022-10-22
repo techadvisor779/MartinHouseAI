0435cb4abfd4d98383c28f828672fdf88523d6bb
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-21 19:44:09 -0700

    Update pollock.js

--- pollock.js
+++ pollock.js
@@ -554,7 +554,7 @@ function submitForm() {
      c.textAlign = 'right';
      var h3s = document.getElementsByTagName("h3");
      if (h3s) {
-          h3.textContent = val;
+          pass;
      } else {
           var h3 = document.createElement('h3');          
           h3.style.transform = "translate( 850px, -100px) rotate( -10deg)";
