6c4d801f72df552a96e00a53a4d71f9d89d76c8f
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-22 19:30:49 -0700

    Update mandrian.js

--- mandrian.js
+++ mandrian.js
@@ -131,16 +131,18 @@ var draw = function(e) {
 }
 
 function submitForm() {
-     div.parentNode.removeChild(h3);
-     var val = document.getElementById('signIt').value;
-     val = val + " '22" ;
-     c.fillStyle = 'Black';
-     c.font = "18pt papyrus";
-     c.textAlign = 'right';
-     var h3 = document.createElement('h3');          
-     h3.style.transform = "translate( 800px, -100px) rotate( -10deg)";
-     h3.textContent = val; 
-     document.body.appendChild(h3);
+    if (h3) {
+        h3.parentNode.removeChild(h3);
+    }
+    var val = document.getElementById('signIt').value;
+    val = val + " '22" ;
+    c.fillStyle = 'Black';
+    c.font = "18pt papyrus";
+    c.textAlign = 'right';
+    var h3 = document.createElement('h3');          
+    h3.style.transform = "translate( 800px, -100px) rotate( -10deg)";
+    h3.textContent = val; 
+    document.body.appendChild(h3);
 }
 
 var lineClick = function() {
