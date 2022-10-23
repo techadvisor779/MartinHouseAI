b02567d9acbab53b27ee381fba2ff30e2ac73dd1
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-22 23:06:36 -0700

    Add files via upload

---
+++ monet.js
@@ -0,0 +1,45 @@
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+
+var size = 600;
+var dpr = window.devicePixelRatio;
+canvas.width = size * dpr;
+canvas.height = size * dpr;
+
+document.getElementById('file-input').onchange = function () {
+  loadImage(
+    this.files[0],
+    function (img) {
+      document.body.appendChild(img)
+    },
+    { maxWidth: 600 } // Options
+  )
+}
+
+let img;
+  
+function preload() {
+  img = loadImage('sample-image.png');
+}
+  
+function setup() {
+  image(img, 20, 40, 100, 100);
+}
+
+function submitForm() {
+    if (h3) {
+        h3.parentNode.removeChild(h3);
+    }
+    var val = document.getElementById('signIt').value;
+    val = val + " '22" ;
+    c.fillStyle = 'Black';
+    c.font = "12pt papyrus";
+    c.textAlign = 'right';
+    var h3 = document.createElement('h3');          
+    h3.style.transform = "translate( 800px, -100px) rotate( -10deg)";
+    h3.textContent = val; 
+    document.body.appendChild(h3);
+}
+
+draw();
+canvas.addEventListener('mousedown', draw);
