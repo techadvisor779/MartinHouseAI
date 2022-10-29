2c09a0296c51554358809b800fdd5300ea8fd6fa
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2022-10-29 15:06:29 -0700

    Update monet.js

--- monet.js
+++ monet.js
@@ -6,8 +6,23 @@ var dpr = window.devicePixelRatio;
 canvas.width = size * dpr;
 canvas.height = size * dpr;
 
-let img = loadImage('images/Claude-Monet-770x736.jpg');
+let img = new Image;
+img.src = "images/Claude-Monet-770x736.jpg";
 c.drawImage(img,0,0);
 
+function submitForm() {
+    if (h3) {
+        h3.parentNode.removeChild(h3);
+    }
+    var val = document.getElementById('signIt').value;
+    val = val + " '22" ;
+    c.fillStyle = 'Black';
+    c.font = "12pt papyrus";
+    c.textAlign = 'right';
+    var h3 = document.createElement('h3');          
+    h3.style.transform = "translate( 800px, -100px) rotate( -10deg)";
+    h3.textContent = val; 
+    document.body.appendChild(h3);
+}
 
 canvas.addEventListener('mousedown', draw);
