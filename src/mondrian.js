2dbfafdbb01398b508ee8de1d77f2346b2b3bebd
Alan Martin <52461073+techadvisor779@users.noreply.github.com>
2023-01-14 14:48:29 -0800

    Update mondrian.js

--- src/mondrian.js
+++ src/mondrian.js
@@ -4,7 +4,7 @@ var c = canvas.getContext('2d');
 var dpr = window.devicePixelRatio;
 canvas.width = window.innerWidth*dpr*.34;
 canvas.height = window.innerWidth*dpr*.22;
-var size = canvas.width;
+var size = canvas.width*.7255;
 //console.log( dpr, canvas.width, canvas.height);
 //c.scale(dpr, dpr);
 c.lineWidth = 6;
